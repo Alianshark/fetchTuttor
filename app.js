bb11e46b53c5eb9db44d0105e054125c71e905d4
Enormouse <johnnysalvia@gmail.com>
2023-04-07 19:09:18 +0300

    add on screenPage logo and login

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+document.body.innerHTML = 'Loading';
+const url = `https://api.github.com/users/DarthVanger`;
 async function requestGithub(url) {
-    const response = await fetch(url, {
-      headers: {
-        'Authorization': 'Bearer ' + token,
-      },
-    });
-  
-    if (!response.ok) {
-      const responseJson = await response.json();
-      throw Error(responseJson?.message || response.status);
-    }
-  
-    const responseJson = await response.json();
-    console.log('Response from Github: ', responseJson);
-    return responseJson;
+  const response = await fetch(url, {
+    headers: {
+      'Authorization': 'Bearer ' + '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',
+    },
+  });
+
+  if (!response.ok) {
+    console.log('error', response);
   }
+
+  const responseJson = await response.json();
+  console.log('Response from Github: ', responseJson);
+  return responseJson;
+}
+
+let userInfo = await requestGithub(url);
+console.log('userInfo: ',userInfo.avatar_url);
+document.body.innerHTML = `<h1>${userInfo.login} </h1>` + `<img src=${userInfo.avatar_url}/>`;
