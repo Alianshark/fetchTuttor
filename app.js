a1e0318d47870f9c2dc6202b442251b613d31064
Enormouse <johnnysalvia@gmail.com>
2023-04-08 13:19:24 +0300

    server

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 document.body.innerHTML = 'Loading';
-const url = `https://api.github.com/users/DarthVanger`;
+const url = `localhost:3000`;
+
 async function requestGithub(url) {
   const response = await fetch(url, {
     headers: {
-      'Authorization': 'Bearer ' + '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',
+      'Authorization': 'Bearer ' + 'token',
     },
   });
 
@@ -17,5 +18,3 @@ async function requestGithub(url) {
 }
 
 let userInfo = await requestGithub(url);
-console.log('userInfo: ',userInfo.avatar_url);
-document.body.innerHTML = `<h1>${userInfo.login} </h1>` + `<img src=${userInfo.avatar_url}/>`;
